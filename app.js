1bcd2e60922ba70201b2d4813b32eaa867e56bc6
kynov007 <mochdickyn@gmail.com>
2024-09-06 14:08:07 +0700

    feat: add Home Page

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const addressBook = {
       fullName: "John Doe",
       phone: "1234567890",
       email: "john@example.com",
+      address: "1234 Main St, Anytown, USA",
     },
     {
       id: 2,
       fullName: "Jane Smith",
       phone: "0987654321",
       email: "jane@example.com",
+      address: "5678 Oak Ave, Other City, USA",
     },
   ],
 };
